a3794b25d57b1641bc78b9ec0beec592253147e4
Isabella Zhong <isabellazhong888@gmail.com>
2025-09-08 14:03:41 -0400

    add reduce

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        reduce(100);
+    }
+
+    public static int reduce(int n) {
+        int counter = 0;
+        while (n > 0) {
+            if (n%2==0) {
+                n = n/2;
+            } else {
+                n -= 1;
+            }
+            counter++;
+        }
+        System.out.println(counter);
+        return counter;
+    }
+}
