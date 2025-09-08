8870090c267a44df3cb9347864cfb0073f9b82c7
Isabella Zhong <isabellazhong888@gmail.com>
2025-09-08 12:53:07 -0400

    add default test

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,6 +5,10 @@ public class Multiples {
         multiples(1000,3,5);
     }
 
+    public static int multiples() {
+        return multiples(1000,3,5);
+    }
+
     public static int multiples(int m, int a, int b) {
         int counter = 0;
         for (int i=1; i<m; i++) {
